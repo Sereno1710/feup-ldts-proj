25efca8b25472416780390b7ddab47565c1a1b14
Bruno Fernandes <up202108871@up.pt>
2022-11-23 17:53:47 +0000

    Changed GUI interface to LanternaGUI class and implemented some methods

--- src/main/java/ChickenViewer.java
+++ src/main/java/ChickenViewer.java
@@ -1,6 +1,6 @@
 public class ChickenViewer implements ElementViewer<Chicken>{
     @Override
-    public void draw(Chicken chicken, GUI gui) {
-        gui.drawChicken(chicken.getPosition());
+    public void draw(Chicken chicken, LanternaGUI lanternaGui) {
+        lanternaGui.drawChicken(chicken.getPosition());
     }
 }
--- src/main/java/TreeViewer.java
+++ src/main/java/TreeViewer.java
@@ -1,6 +1,6 @@
 public class TreeViewer implements ElementViewer<Tree>{
     @Override
-    public void draw(Tree tree, GUI gui) {
+    public void draw(Tree tree, LanternaGUI lanternaGui) {
         //gui.drawTree(tree);
     }
 }
