3c9e4f9b8d11050d269f1d11a2fdf214961bcf27
José Santos <sjse7780@gmail.com>
2022-11-25 14:43:44 +0000

    Fixed Chicken Tests

--- src/test/java/ChickenTests.java
+++ src/test/java/ChickenTests.java
@@ -1,30 +1,40 @@
+import com.aor.crossyroad.controller.game.ChickenController;
 import com.aor.crossyroad.model.Position;
 import com.aor.crossyroad.model.game.arena.Arena;
 import com.aor.crossyroad.model.game.elements.Chicken;
+import com.aor.crossyroad.model.game.lines.Line;
+import com.aor.crossyroad.model.game.lines.Sidewalk;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.security.PrivateKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChickenTests {
+    private ChickenController controller;
     private Chicken chicken;
     private Arena arena;
     @BeforeEach
     public void FillAll(){
-        arena= new Arena(10,10);
-        chicken = new Chicken(5,5);
+        arena= new Arena(20,20);
+        chicken = new Chicken(15,16);
         arena.setChicken(chicken);
+        controller = new ChickenController(arena);
+        arena.setLines(Arrays.asList());
     }
     @Test
     public void ChickenMoveUp(){
-        arena.moveChicken(chicken.MoveUp());
-        assertEquals(new Position(5,4),chicken.getPosition());
+        controller.moveChickenUp();
+        assertEquals(new Position(15,15),chicken.getPosition());
     }
     @Test
     public void ChickenCantMoveUp(){
-        chicken.setPosition(new Position(1,0));
-        arena.setChicken(chicken);
-        arena.moveChicken(chicken.MoveUp());
-        assertEquals(new Position(1,0),chicken.getPosition());
+        arena.setLines(Arrays.asList(new Sidewalk(15)));
+        controller.moveChickenUp();
+        assertEquals(new Position(15,16),chicken.getPosition());
     }
 }
