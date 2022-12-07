7bc5a448e55240b9cadb72fb89f1fa2d972bc996
Bruno Fernandes <up202108871@up.pt>
2022-12-07 11:55:44 +0000

    Added tree randomizing when the chicken gets to a new safezone

--- src/main/java/com/aor/crossyroad/controller/game/ChickenController.java
+++ src/main/java/com/aor/crossyroad/controller/game/ChickenController.java
@@ -36,9 +36,15 @@ public class ChickenController extends GameController {
 
     @Override
     public void step(Game game, LanternaGUI.ACTION action, long time) throws IOException {
-        if (action == LanternaGUI.ACTION.UP) moveChickenUp();
+        if (action == LanternaGUI.ACTION.UP) {
+            moveChickenUp();
+            getModel().setLastSafe(getModel().getChicken());
+        }
         if (action == LanternaGUI.ACTION.RIGHT) moveChickenRight();
-        if (action == LanternaGUI.ACTION.DOWN) moveChickenDown();
+        if (action == LanternaGUI.ACTION.DOWN) {
+            moveChickenDown();
+            getModel().setLastSafe(getModel().getChicken());
+        }
         if (action == LanternaGUI.ACTION.LEFT) moveChickenLeft();
     }
 }
