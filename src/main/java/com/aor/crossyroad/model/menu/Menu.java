8cac1727258839b0e11e3716f49aef41b5045669
José Santos <sjse7780@gmail.com>
2022-12-13 06:51:58 +0000

    Cleared Menu and GameOver classes

--- src/main/java/com/aor/crossyroad/model/menu/Menu.java
+++ src/main/java/com/aor/crossyroad/model/menu/Menu.java
@@ -2,34 +2,7 @@ package com.aor.crossyroad.model.menu;
 import java.util.Arrays;
 import java.util.List;
 public class Menu extends MenuPhases{
-    private int currentOption = 0;
-
     public Menu() {
         super(Arrays.asList("Start","Exit"));
-    }
-
-    @Override
-    public void nextOption() {currentOption++;if (currentOption > getNumberOptions()) currentOption = 0;}
-    @Override
-    public void previousOption() {currentOption--;if (currentOption < 0) currentOption = getNumberOptions()-1;}
-    @Override
-    public String getOption(int i) {
-        return getOptions().get(i);
-    }
-    @Override
-    public boolean isSelected(int i) {
-        return currentOption == i;
-    }
-    @Override
-    public boolean isSelectedExit() {
-        return isSelected(1);
-    }
-    @Override
-    public boolean isSelectedStart() {
-        return isSelected(0);
-    }
-    @Override
-    public int getNumberOptions() {
-        return this.getOptions().size();
     }
 }
--- src/main/java/com/aor/crossyroad/model/menu/MenuPhases.java
+++ src/main/java/com/aor/crossyroad/model/menu/MenuPhases.java
@@ -9,11 +9,6 @@ public abstract class MenuPhases {
     public MenuPhases(List<String> options){
         this.options=options;
     }
-
-    public List<String> getOptions() {
-        return options;
-    }
-
     public void nextOption() {currentOption++;if (currentOption > getNumberOptions()) currentOption = 0;}
     public void previousOption() {currentOption--;if (currentOption < 0) currentOption = getNumberOptions()-1;}
     public String getOption(int i) {
